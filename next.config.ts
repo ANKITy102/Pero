3e2882ce14cae2ec73e546d4ef23b3c246959766
Ankit Yadav <mahesh9426346889@gmail.com>
2025-05-18 15:33:32 +0530

    build fixes

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   images: {
-    domains: [
-      "lh3.googleusercontent.com",
-      "cdn.pixabay.com"
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "cdn.pixabay.com",
+        port: "",
+        pathname: "/**",
+      },
     ],
   },
 };
